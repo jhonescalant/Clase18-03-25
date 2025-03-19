960609202cba4b73d15d68086fc0a0a4fc511ad0
jhonescalant <jhonescalante2019@gmail.com>
2025-03-18 21:25:38 -0300

    Clase pokemon

--- Program.cs
+++ Program.cs
@@ -6,5 +6,30 @@
         {
             Console.WriteLine("Hello, World!");
         }
+
     }
+
+    public class pokemon 
+    {
+        // Atributos
+        public string nombre;
+        public int salud;
+        public string raza;
+
+        //Metodos
+        public void atacar() 
+        {
+            Console.WriteLine(" Ataco !");
+        }
+
+        public void defender()
+        {
+            Console.WriteLine("Activo defensa");
+        
+        }
+
+    }
+
+
+
 }
