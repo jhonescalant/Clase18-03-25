10abb422564ab60b92677059ed5570b405772ebf
jhonescalant <jhonescalante2019@gmail.com>
2025-03-18 21:07:38 -0300

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Clase18_03_25
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
